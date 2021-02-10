deb7facf644e8a56590ef9836ab8568a9e78c0c9
Supriyo Paul <paulsupriyo64nk@gmail.com>
2021-02-10 20:47:10 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-import 'package:flutter_30_days/homepage.dart';
+import 'package:flutter_30_days/pages/homepage.dart';
+import 'package:flutter_30_days/pages/loginPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,19 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = "Hello!";
-    bool isMale = true;
-    num temp = 30.1;
-    double foo = 30.1112;
-    num number = 123;
-    var nam = "Supriyo";
-    var age = 12;
-    const pi = 3.14;
-    final lo = 123;
-
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
