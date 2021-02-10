9f4d228def0a4a4e26d3517dd9c06ec177f176f2
Supriyo Paul <paulsupriyo64nk@gmail.com>
2021-02-10 19:35:27 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import "package:flutter/material.dart";
+import 'package:flutter_30_days/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Hello!";
+    bool isMale = true;
+    num temp = 30.1;
+    double foo = 30.1112;
+    num number = 123;
+    var nam = "Supriyo";
+    var age = 12;
+    const pi = 3.14;
+    final lo = 123;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter 30 days"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
